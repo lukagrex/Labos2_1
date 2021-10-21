508c185830ff0937329a6874cab2889df62274db
Luka Gregorić <endo145@gmail.com>
2021-10-21 18:16:23 +0200

    Add project files.

---
+++ Labos2_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labos2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KompleksniBrojKlasa kbk1 = new KompleksniBrojKlasa(2, 4);
+            KompleksniBrojStruktura kbs1 = new KompleksniBrojStruktura(3, 5);
+            
+            KompleksniBrojKlasa kbk2 = kbk1;
+            KompleksniBrojStruktura kbs2 = kbs1;
+
+            Console.WriteLine($"Kbk1 = {kbk1}");
+            Console.WriteLine($"Kbk2 = {kbk2}");
+            Console.WriteLine($"Kbs1 = {kbs1}");
+            Console.WriteLine($"Kbs2 = {kbs2}");
+
+            kbk2.RealniDio = 25;
+            kbs2.ImaginarniDio = 25;
+
+            Console.WriteLine($"Kbk1 = {kbk1}");
+            Console.WriteLine($"Kbk2 = {kbk2}");
+            Console.WriteLine($"Kbs1 = {kbs1}");
+            Console.WriteLine($"Kbs2 = {kbs2}");
+            Console.ReadKey(false);
+        }
+    }
+}
---
+++ Labos2_1/Struktura.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labos2_1
+{
+    struct KompleksniBrojStruktura
+    {
+       
+        public KompleksniBrojStruktura(double realni, double imaginarni)
+        {
+            RealniDio = realni;
+            ImaginarniDio = imaginarni;
+        }
+
+        public double RealniDio;
+        public double ImaginarniDio;
+
+        public override string ToString()
+        {
+            return $"({RealniDio}, {ImaginarniDio})";
+        }
+    }
+}
